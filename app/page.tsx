965336236d572835d5327fe0c81f413134bd3fc0
Tobias Meyhöfer <s87766@bht-berlin.de>
2024-04-02 23:24:06 +0200

    test

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
       <main className="w-full h-full flex justify-center items-center flex-col gap-6 bg-slate-100">
         <h1 className="font-bold text-5xl">YOURNAL</h1>
         <p className="text-xl">Mehr als nur ein Tagebuch</p>
+        <input placeholder="hier rein">
+        
+        </input>
       </main>
     </>
   );
