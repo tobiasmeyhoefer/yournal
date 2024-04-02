76dc27002953f7f67ccf58bf957733d51acaca17
Tobias Meyhöfer <s87766@bht-berlin.de>
2024-04-02 21:06:10 +0200

    starting page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
     <>
-      <h1>YOURNAL</h1>
-      <p>Mehr als nur ein Tagebuch</p>
+      <main className="w-full h-full flex justify-center items-center flex-col gap-6 bg-slate-100">
+        <h1 className="font-bold text-5xl">YOURNAL</h1>
+        <p className="text-xl">Mehr als nur ein Tagebuch</p>
+      </main>
     </>
   );
 }
